230d658625058b7ff59fe9883471a4fbc4db3e14
Reagan Roop <roopc@mail.missouri.edu>
2023-10-24 12:44:54 -0500

    Initial lab05

---
+++ lab05/forward.py
@@ -0,0 +1,47 @@
+import numpy as np
+import math
+
+#thetas = [0, 0, 0, 0, 0, 0]
+thetas = [30.003, 29.998, 29.997, 29.999, 29.997, 29.995]
+thetas = np.radians(thetas)
+
+dh_array = np.array([[thetas[0], 0, 0, -math.pi/2],
+                     [thetas[1], ((2 + 7/8)+(2 + 1/16)) * 25.4, 8 * 25.4, 0],
+                     [thetas[2], 0, 0, math.pi/2],
+                     [thetas[3], 8 * 25.4, 0, -math.pi/2],
+                     [thetas[4], 0, 0, math.pi/2],
+                     [thetas[5], 2.202* 25.4, 0, 0]])
+
+def A_matrix(theta, d, a, alpha):
+    matrix = np.array([[math.cos(theta), -math.cos(alpha) * math.sin(theta), math.sin(alpha) * math.sin(theta), a * math.cos(theta)],
+                       [math.sin(theta), math.cos(alpha) * math.cos(theta), -math.sin(alpha) * math.cos(theta), a * math.sin(theta)],
+                       [0, math.sin(alpha), math.cos(alpha), d],
+                       [0,0,0,1]])
+    return matrix
+
+def calc_T():
+    matrix = np.identity(4)
+
+    for i in range(6): # int i = 0; i < 6; i++
+        (theta, d, a, alpha) = tuple(dh_array[i])
+        Amatrix = A_matrix(theta, d, a, alpha)
+        matrix = np.dot(matrix, Amatrix)
+
+    return matrix
+
+def T_to_OAT(T_matrix):
+    tool = np.degrees(math.atan2(T_matrix[2][1], -T_matrix[2][0]))
+    orientation = math.atan2(T_matrix[0][2], -T_matrix[1][2])
+    approach = np.degrees(math.atan2(-T_matrix[2][2], T_matrix[0][2] / math.sin(orientation)))
+
+    orientation = np.degrees(orientation)
+
+    return orientation, approach, tool
+
+Tmatrix = calc_T()
+x = Tmatrix[0][3]
+y = Tmatrix[1][3]
+z = Tmatrix[2][3]
+
+print(T_to_OAT(Tmatrix))
+print(x, y, z)
