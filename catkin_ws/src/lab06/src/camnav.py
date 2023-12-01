0d5c41308b2611509e8c08eb61e0ea704c1930f9
Reagan Roop <roopc@mail.missouri.edu>
2023-12-01 13:13:38 -0600

    Progressed lab06 work and labgroup's file

--- catkin_ws/src/lab06/src/camnav.py
+++ catkin_ws/src/lab06/src/camnav.py
@@ -2,6 +2,7 @@
 import rospy
 
 from geometry_msgs.msg import Twist
+from ar_pose.msg import ARMarker, ARMarkers
 
 import numpy
 import tf
@@ -13,32 +14,56 @@ polarCoords = [0,0,0]
 mHr = numpy.identity(4)
 
 rHc = numpy.identity(4)
-rHc[0,3] = 0.067357083
+rHc[0,3] = 0.015
 rHc[1,3] = -0.009439617
-rHc[2,3] = 0.211620667
+# rHc[1,3] = -0.023
+rHc[2,3] = 0.235 - 0.05
 
 # Gains
-GAIN_RHO = 0.3
-GAIN_ALPHA = 0.8
+GAIN_RHO = 0.2
+GAIN_ALPHA = 0.35
 GAIN_BETA = -0.15
 
 # GAIN_ALPHA = 0.4
 # GAIN_BETA = -0.05
+xyz = [0,0,0]
+rpy = [0,0,0]
+first_ar = True
+ar_bias = 0
+ar_received = False
 
-x = 0
-y = 0
+marker_in_view = False
 
+quat2 = tf.transformations.quaternion_from_euler(-1.67493, 0.01726275, -1.575038333, axes='sxyz')
 
-# quat2 = tf.transformations.quaternion_from_euler(-1.67493, 0.01726275, -1.575038333, axes='sxyz')
-# euler2 = tf.transformations.euler_from_quaternion(quat2)
-# rotation_matrix2 = numpy.array([[math.cos(euler2[2]), -math.sin(euler2[2]), 0],
-#                         [math.sin(euler2[2]), math.cos(euler2[2]), 0],
-#                         [0, 0, 1]])
-# rHc[:3,:3] = rotation_matrix2
+rHc[:3,:3] = tf.transformations.quaternion_matrix(quat2)[:3,:3]
 
 '''
 HELPER FUNCTIONS
 '''
+def ar_callback(data):
+    global xyz, rpy, first_ar, ar_bias, ar_received, marker_in_view
+
+    if len(data.markers) > 0:
+        position = data.markers[0].pose.pose.position
+        xyz = [position.z, position.y, -position.x]
+
+        orientation = data.markers[0].pose.pose.orientation
+        orientation = [orientation.z, orientation.y, -orientation.x, orientation.w]
+        rpy = list(tf.transformations.euler_from_quaternion(orientation))
+
+        marker_in_view = True
+    else:
+        marker_in_view = False
+        return
+
+    if first_ar == True:
+        ar_bias = rpy[2]
+        first_ar = False
+
+    rpy[2] -= ar_bias
+    ar_received = True
+
 # Makes sure an angle is between -pi and +pi
 def normalizeAngle(angle):
     angle = angle % (2 * math.pi)
@@ -48,25 +73,26 @@ def normalizeAngle(angle):
 
 # Calculates current rho alpha and beta and stores in global 'polarCoords'
 def cart2pol():
-    global polarCoords, mHr, x, y
+    global polarCoords, mHr
+    rHm = numpy.linalg.inv(mHr)
 
+    x = rHm[0,3]
+    y = rHm[1,3]
 
-    rotation_matrix = numpy.identity(3)
-    rotation_matrix[:3,:3] = mHr[:3,:3]
-
-    euler = tf.transformations.euler_from_matrix(rotation_matrix)
+    euler = tf.transformations.euler_from_matrix(rHm[:3,:3], 'rxyz')
 
     rho = numpy.sqrt(x ** 2 + y ** 2)
-    alpha = normalizeAngle(math.atan2(-y, -x) - euler[2])
+    alpha = math.atan2(-y, -x) - euler[2]
     
-    if (alpha > math.pi/2):
-        alpha -= math.pi
-    elif (alpha <= -math.pi):
-        alpha += math.pi  
-    
+    alpha = alpha % (2 * math.pi)
+
+    if (alpha > math.pi):
+        alpha -= 2*math.pi
+
     beta = -alpha - euler[2]
 
     polarCoords[:3] = rho, alpha, beta
+
 
 
 
@@ -77,9 +103,10 @@ if __name__=='__main__':
 
     # Initialize node
     rospy.init_node('camnav')
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(1)
 
     vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+    rospy.Subscriber('/ar_pose_marker', ARMarkers, ar_callback)
     listener = tf.TransformListener()
 
     vel_msg = Twist()
@@ -91,63 +118,77 @@ if __name__=='__main__':
         mHc = numpy.identity(4)
 
         try:
+            listener.waitForTransform("camera", "4x4_2", rospy.Time(0), rospy.Duration(4))
+            # now = rospy.Time.now()
+            # listener.waitForTransform("camera", "4x4_2", now, rospy.Duration(4))
             (trans, quat) = listener.lookupTransform("camera", "4x4_2", rospy.Time(0))
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             rospy.logerr('cant find')
             rate.sleep()
             continue
-        # except Exception as error:
-        #     print(error)
+        except Exception as e:
+            rospy.logerr(e)
+            rate.sleep()
+            continue
 
-        if llTrans == trans and llQuat == quat:
-            rospy.logerr("Lost Tracking")
-            break
+
+        # except Exception as error
+
+        # if llTrans == trans and llQuat == quat:
+        #     rospy.logerr("Lost Tracking")
+        #     break
 
         llTrans = trans
         llQuat = quat
-        
+
+
         euler = tf.transformations.euler_from_quaternion(quat)
-        rotation_matrix = numpy.array([[math.cos(euler[2]), -math.sin(euler[2]), 0],
-                                [math.sin(euler[2]), math.cos(euler[2]), 0],
-                                [0, 0, 1]])
-
-
 
         mHc[:3,3] = numpy.array(trans)
-        mHc[:3,:3] = rotation_matrix
+        mHc[:3,:3] = tf.transformations.euler_matrix(euler[0],euler[1],euler[2])[:3,:3]
         
         os.system('clear')
         mHr = numpy.dot(mHc, numpy.linalg.inv(rHc)) # Robot w.r.t the marker (or marker to robot)
 
 
-        x = -numpy.abs(mHr[2,3])
-        y = mHr[1,3]
+        # x = -numpy.abs(mHr[2,3])
+        # y = mHr[1,3]
 
-        x = x + 0.250 # Adjust stop position to not collide with marker
-
+        # print(mHr)
+        # print(numpy.linalg.inv(mHr))
 
         # print('x:', x)
         # print('y:', y)
         # print('z:', mHr[0,3])
+        # print(numpy.linalg.inv(mHr))
 
         cart2pol()
 
-        # print(polarCoords)
+        # print(polarCoords[0])
+        # print(math.degrees(polarCoords[1]))
+        # print(math.degrees(polarCoords[2]))
 
         vel_msg.linear.x = GAIN_RHO * polarCoords[0]
         vel_msg.angular.z = (GAIN_ALPHA * polarCoords[1]) + (GAIN_BETA * polarCoords[2])
         vel_pub.publish(vel_msg)
 
-        print('Linear Velocity:',vel_msg.linear.x)
-        print('Angular Velocity:',vel_msg.angular.z)
+        # print('Linear Velocity:',vel_msg.linear.x)
+        # print('Angular Velocity:',vel_msg.angular.z)
+
+        print(xyz)
+        print(rpy)
+        print(first_ar)
+        print(ar_received)
+
+
 
 
                 # Stop when close enough to threshold
         pos_thresh = 0.05
         angle_thresh = 0.15
 
-        if abs(mHr[0,3]) <= pos_thresh and abs(mHr[1,3]) <= pos_thresh:
-            rospy.logwarn("At Goal")
-            break
+        # if abs(x) <= pos_thresh and abs(y) <= pos_thresh:
+        #     rospy.logwarn("At Goal")
+        #     break
             
         rate.sleep()
